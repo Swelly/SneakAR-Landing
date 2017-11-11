dfec913051a5c7e56daf4fd8d78000b016dc84ab
Swelly <surfswell17@gmail.com>
2017-11-10 17:21:56 -0800

    done

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,26 @@ import status from './test';
 (() => {
     console.log(`Test:${status()}`);
 })();
+
+$(document).ready(function() {
+
+	function setHeight() {
+		let windowHeight = $(window).innerHeight();
+		$('#app').css('min-height', windowHeight);
+	};
+
+	setHeight();
+
+	$(window).resize(function() {
+		setHeight();
+	});
+
+
+	$(document).on("mousemove",function(e) {
+		let card = $("#app");
+		let ax = -($(window).innerWidth()/2- e.pageX) / 20;
+		let ay = ($(window).innerHeight()/2- e.pageY) / 10;
+
+		card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
+	});
+});
